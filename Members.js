3ff8416caec2c9d76bafd4fc5e78df43d7bd5600
Elizabeth Joseph <lizzy22.mj@gmail.com>
2022-11-04 18:52:16 +0100

    Commited all files

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+	{
+		id: 1,
+		name: 'Elizabeth Joseph',
+		email: 'lizzy22.mj@gmail.com',
+		status: 'active'
+	},
+	{
+		id: 2,
+		name: 'Esther Joseph',
+		email: 'Estherjoseph@gmail.com',
+		status: 'inactive'
+	},
+	{
+		id: 3,
+		name: 'Joy Ekwe',
+		email: 'ekwejoy@gmail.com',
+		status: 'active'
+	}
+];
+
+module.exports = members;
